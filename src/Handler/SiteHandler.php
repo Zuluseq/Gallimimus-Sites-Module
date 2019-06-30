639421030d8cbdb457c0e897f866f2f31626a79c
vizmedia <maciej@vizmedia.pl>
2019-06-30 20:56:07 +0200

    site handler build

--- src/Handler/SiteHandler.php
+++ src/Handler/SiteHandler.php
@@ -22,21 +22,63 @@ class SiteHandler implements RequestHandlerInterface
     {
 		$url = $request->getServerParams()['HTTP_HOST'];
 		$sch = $request->getServerParams()['REQUEST_SCHEME'];
-		$uri = $sch.'://'.$url.'/repository/get/getSiteBySlugAndNamespace?namespace=vizmedia-test&slug=home';
+		$uriSzablon = $sch.'://'.$url.'/repository/get/getSiteBySlugAndNamespace?namespace=vizmedia-test&slug=home';
+		$uriSekcje = $sch.'://'.$url.'/repository/get/getSectionsOfSite?id_site=';
+		$uriKomponenty = $sch.'://'.$url.'/repository/get/getComponentsOfSection?id_site={id_site}&section={section}';
 
  		$client = new Client();
         $client->setAdapter('Zend\Http\Client\Adapter\Curl');
          
-        $client->setUri($uri);
+		// pobieram szablon
+        $client->setUri($uriSzablon);
 		$client->setOptions(array(
 			'maxredirects' => 0,
 			'timeout'      => 30
 		));
         $result = $client->send();
         $JsonBody = $result->getBody();
-
 		$phpNative = Json::decode($JsonBody);
+		$site = $phpNative[0];
+		$szablon = $site->template;
 
-        return new HtmlResponse($phpNative->results);
+		// pobieram komponenty
+		$uriSekcje = $uriSekcje.$site->id_site;
+        $client->setUri($uriSekcje);
+        $result = $client->send();
+        $JsonBody = $result->getBody();
+		$sekcje = Json::decode($JsonBody);
+
+		$sekcjeContent = array();
+		// tworze content dla sekcji i wypełniam je komponentami
+		$wynik = "";
+		foreach($sekcje as $sekcja)
+		{
+			$uriKomp = str_replace('{id_site}',$site->id_site,$uriKomponenty);
+			$uriKomp = str_replace('{section}',$sekcja->section,$uriKomp);
+			$client->setUri($uriKomp);
+			$result = $client->send();
+			$JsonBody = $result->getBody();
+			$komponenty = Json::decode($JsonBody);
+
+			// $sekcjeContent[] = $this->preparujKomponenty($komponenty);
+			$wynik = $wynik.$this->preparujKomponenty($komponenty);
+		}
+
+
+		// wstawiam sekcje do szablonu
+
+        return new HtmlResponse($wynik);
+        // return new JsonResponse($sekcjeContent,200,[],JSON_PRETTY_PRINT);
+        // return new JsonResponse($site->template);
     }
+
+	private function preparujKomponenty($komponenty)
+	{
+		$wynik = "";
+		foreach($komponenty as $komponent) 
+		{
+			$wynik = $wynik.$komponent->template;
+		}
+		return $wynik;
+	}
 }
